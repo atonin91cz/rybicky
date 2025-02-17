2a468fd58d180e58af66cd151a0735039c1bf0a1
atonin91cz <antonin91cz@seznam.cz>
2025-02-17 12:46:35 +0100

    Nový build pro GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/akvarium/", // Název tvého GitHub repozitáře
+  base: "/rybicky/", // ⚠️ Důležité! Nastav správnou cestu podle názvu repozitáře
   plugins: [react()],
 });
